6f6e52d1833bdfe789676799bde54aa69e729b60
Michael Wolf <susonicth@gmail.com>
2022-12-15 01:38:20 +0100

    use sum and for in lamda in python; added part 02-2

--- 02/02-2.groovy
+++ 02/02-2.groovy
@@ -2,9 +2,9 @@ def WIN = 6
 def DRAW = 3
 def LOOSE = 0
 def map = [
-    A:[X:1+DRAW  ,Y:2+WIN   ,Z:3+LOOSE],
-    B:[X:1+LOOSE ,Y:2+DRAW  ,Z:3+WIN],
-    C:[X:1+WIN   ,Y:2+LOOSE ,Z:3+DRAW],
+    A:[X:3+LOOSE ,Y:1+DRAW ,Z:2+WIN],
+    B:[X:1+LOOSE ,Y:2+DRAW ,Z:3+WIN],
+    C:[X:2+LOOSE ,Y:3+DRAW ,Z:1+WIN],
 ]
 def score = 0
 new File("input.txt").eachLine{ line->
