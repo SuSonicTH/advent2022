a073ae7a8b93486d5b3238e2c22538b7a49603b6
Wolf,Michael <Michael.Wolf@drei.com>
2022-12-16 15:02:13 +0100

    added 6.1

---
+++ 06/06-1.py
@@ -0,0 +1,18 @@
+def isStart(s, string):
+    for i in range(3):
+        index = string.index(string[s+i], s+i+1)
+        if index > 0 and index < s+4:
+            return False
+    return True
+
+def getMarkerPosition(data):
+    for i in range(len(data) - 4):
+        if isStart(i, data):
+            return i + 4
+
+
+with open('input.txt', 'r') as file:
+    data = file.read()
+
+print(f"marker position {getMarkerPosition(data)}")
+
